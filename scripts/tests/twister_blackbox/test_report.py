eb6daa8e8a20aee84311cbe88fcd3ff67fc283a8
Dmitrii Golovanov <dmitrii.golovanov@intel.com>
2024-10-17 09:02:35 +0200

    twister: ztest: harness: Fix missed TestCase statuses
    Fix a problem of Ztest suite names not taken into account by Twister
    to identify a TestCase, so in some situations a Ztest test's status
    was not assigned to the proper TestCase and it remains 'None'
    whereas the actual status value lost, eventually the resulting total
    execution counters not correct.
    
    The issue was observed in these situations:
     * Ztest application with multiple test suites having same test names.
     * Ztest suite is 'skipped' entirely on execution with all its tests.
    
    The proposed solution extends Twister test case name for Ztest to
    include Ztest suite name, so the resulting identifier looks like:
       `<test_scenario_name>.<ztest_suite_name>.<ztest_name>`
    
    The above naming scheme now requires ztest_suite_name part to be
    provided for `--sub-test` command line option.
    
    Testcase identifiers in twister.json and testplan.json will also
    include ztest_suite_name component.
    
    The Twister Ztest(Test) Harness is improved to track all state changes
    known from the test application's log for Ztest suites and test cases,
    so now it parses log output from a Ztest application more scurpulously.
    Regular expressions to match log records are extended and optimized
    to compile them only once and, in some cases, fixed (suite summary).
    
    Signed-off-by: Dmitrii Golovanov <dmitrii.golovanov@intel.com>

--- scripts/tests/twister_blackbox/test_report.py
+++ scripts/tests/twister_blackbox/test_report.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2023 Intel Corporation
+# Copyright (c) 2023-2024 Intel Corporation
 #
 # SPDX-License-Identifier: Apache-2.0
 """
@@ -350,12 +350,12 @@ class TestReport:
             (
                 os.path.join(TEST_DATA, 'tests', 'dummy'),
                 ['--detailed-skipped-report'],
-                {'qemu_x86': 5, 'intel_adl_crb': 1}
+                {'qemu_x86': 6, 'intel_adl_crb': 1}
             ),
             (
                 os.path.join(TEST_DATA, 'tests', 'dummy'),
                 ['--detailed-skipped-report', '--report-filtered'],
-                {'qemu_x86': 6, 'intel_adl_crb': 6}
+                {'qemu_x86': 7, 'intel_adl_crb': 7}
             ),
         ],
         ids=['dummy tests', 'dummy tests with filtered']
