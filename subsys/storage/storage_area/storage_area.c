zephyrproject-rtos/zephyr

storage: introduce storage_area
The commit introduces a new way of working with storage for zephyr.
A storage area is an area on flash, eeprom, ram, disk that describes how
this area will be used. It defines the write block size, erase block size
and size of the area and provides methods to read, write, erase and a
ioctl fuction. The write block and erase block sizes that are specified
are not limited to the sizes of the underlying device but they can be a
multiple. A verification option is available to avoid defining area
properties that are not supported by the backing device.

The storage area provides a simple way of working with images and allows
the creation of "matching" images on different devices with different
properties (e.g. erase-block sizes).

The storage area definitions can be extended to provide e.g. encrypted
storage area's or storage area's that are a mix of flash and ram.

The reading and writing routines not only support a classical direct
write, but also a writing in chunks that allows stack saving when
writing or reading data (e.g. when data that consists of multiple
elements need to be written).

On top of the storage area a storage area store is introduced that
divides the storage area into sectors and stores data as records in a
simple and space saving format and ensures data integrity by using a
crc32. The storage area store is not a storage solution, but rather it
is a building block for storing id-data, key-data, ... solutions.
The storage area store adds a configurable "cookie" at the beginning of
each sector that can be used to describe what information can be found
in the store as well as a version, ...
The storage area store also provides support for modifying the first
byte of data in a record. This can be used to invalidate records.

Signed-off-by: Laczen JMS <laczenjms@gmail.com>

subsys/storage/storage_area/storage_area.c
@@ -1,0 +1,131 @@
+/*
+ * Copyright (c) 2024 Laczen
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/storage/storage_area/storage_area.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(storage_area, CONFIG_STORAGE_AREA_LOG_LEVEL);
+
+static bool sa_range_valid(const struct storage_area *area, size_t start,
+			   size_t len)
+{
+	const size_t asize = area->erase_size * area->erase_blocks;
+
+	if ((asize < len) || ((asize - len) < start)) {
+		LOG_DBG("Invalid range");
+		return false;
+	}
+
+	return true;
+}
+
+static size_t sa_chunk_size(const struct storage_area_chunk *ch, size_t cnt)
+{
+	size_t rv = 0U;
+
+	for (size_t i = 0U; i < cnt; i++) {
+		rv += ch[i].len;
+	}
+
+	return rv;
+}
+
+int storage_area_read(const struct storage_area *area, size_t start,
+		      const struct storage_area_chunk *ch, size_t cnt)
+{
+	if ((area == NULL) || (area->api == NULL) || (area->api->read == NULL)) {
+		return -ENOTSUP;
+	}
+
+	const size_t len = sa_chunk_size(ch, cnt);
+
+	if (!sa_range_valid(area, start, len)) {
+		return -EINVAL;
+	}
+
+	return area->api->read(area, start, ch, cnt);
+}
+
+int storage_area_dread(const struct storage_area *area, size_t start, void *data,
+		       size_t len)
+{
+	struct storage_area_chunk rd = {
+		.data = data,
+		.len = len,
+	};
+
+	return storage_area_read(area, start, &rd, 1U);
+}
+
+int storage_area_prog(const struct storage_area *area, size_t start,
+		      const struct storage_area_chunk *ch, size_t cnt)
+{
+	if ((area == NULL) || (area->api == NULL) || (area->api->prog == NULL)) {
+		return -ENOTSUP;
+	}
+
+	const size_t len = sa_chunk_size(ch, cnt);
+
+	if ((!sa_range_valid(area, start, len)) ||
+	    (((len & (area->write_size - 1)) != 0U))) {
+		return -EINVAL;
+	}
+
+	if (STORAGE_AREA_HAS_PROPERTY(area, SA_PROP_READONLY)) {
+		LOG_DBG("prog not supported (read-only)");
+		return -EROFS;
+	}
+
+	return area->api->prog(area, start, ch, cnt);
+}
+
+int storage_area_dprog(const struct storage_area *area, size_t start,
+		       const void *data, size_t len)
+{
+	struct storage_area_chunk wr = {
+		.data = (void *)data,
+		.len = len,
+	};
+
+	return storage_area_prog(area, start, &wr, 1U);
+}
+
+int storage_area_erase(const struct storage_area *area, size_t start,
+		       size_t bcnt)
+{
+	if ((area == NULL) || (area->api == NULL) ||
+	    (area->api->erase == NULL)) {
+		return -ENOTSUP;
+	}
+
+	const size_t ablocks = area->erase_blocks;
+
+	if ((ablocks < bcnt) || ((ablocks - bcnt) < start)) {
+		LOG_DBG("invalid range");
+		return -EINVAL;
+	}
+
+	if (STORAGE_AREA_HAS_PROPERTY(area, SA_PROP_READONLY)) {
+		LOG_DBG("erase not supported (read-only)");
+		return -EROFS;
+	}
+
+	return area->api->erase(area, start, bcnt);
+}
+
+int storage_area_ioctl(const struct storage_area *area,
+		       enum storage_area_ioctl_cmd cmd, void *data)
+{
+	if ((area == NULL) || (area->api == NULL) ||
+	    (area->api->ioctl == NULL)) {
+		return -ENOTSUP;
+	}
+
+	return area->api->ioctl(area, cmd, data);
+}

subsys/storage/storage_area/storage_area_disk.c
@@ -1,0 +1,247 @@
+/*
+ * Copyright (c) 2024 Laczen
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string.h>
+#include <errno.h>
+#include <zephyr/storage/storage_area/storage_area_disk.h>
+#include <zephyr/storage/disk_access.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(storage_area_disk, CONFIG_STORAGE_AREA_LOG_LEVEL);
+
+static int sa_disk_valid(const struct storage_area_disk *disk)
+{
+	int rc = disk_access_init(disk->name);
+
+	if (rc != 0) {
+		return rc;
+	}
+
+	if (IS_ENABLED(CONFIG_STORAGE_AREA_VERIFY)) {
+		size_t scount;
+		size_t ssize;
+
+		rc = disk_access_ioctl(disk->name, DISK_IOCTL_GET_SECTOR_COUNT,
+				       &scount);
+		if (rc != 0) {
+			LOG_DBG("Unable to get disk sector count");
+			return -EINVAL;
+		}
+
+		rc = disk_access_ioctl(disk->name, DISK_IOCTL_GET_SECTOR_SIZE,
+				       &ssize);
+		if (rc != 0) {
+			LOG_DBG("Unable to get disk sector size");
+			return -EINVAL;
+		}
+
+		if (disk->ssize != ssize) {
+			LOG_DBG("Bad disk sector size");
+			return -EINVAL;
+		}
+
+		const struct storage_area *area = &disk->area;
+		const size_t asz = area->erase_blocks * area->erase_size;
+		const size_t esz = scount * ssize;
+
+		if (esz < ((disk->start * ssize) + asz)) {
+			LOG_DBG("Bad area size");
+			return -EINVAL;
+		}
+
+		if ((area->write_size & (ssize - 1)) != 0U) {
+			LOG_DBG("Bad area write size");
+			return -EINVAL;
+		}
+
+		if ((area->erase_size & (ssize - 1)) != 0U) {
+			LOG_DBG("Bad area erase size");
+			return -EINVAL;
+		}
+	}
+
+	return 0;
+}
+
+static int sa_disk_read(const struct storage_area *area, size_t start,
+			const struct storage_area_chunk *ch, size_t cnt)
+{
+	const struct storage_area_disk *disk =
+		CONTAINER_OF(area, struct storage_area_disk, area);
+	size_t starts = start / disk->ssize;
+	size_t bpos = start % disk->ssize;
+	uint8_t buf[disk->ssize];
+	int rc = sa_disk_valid(disk);
+
+	if (rc != 0) {
+		goto end;
+	}
+
+	starts += disk->start;
+	rc = disk_access_read(disk->name, buf, starts, 1U);
+	if (rc != 0) {
+		goto end;
+	}
+
+	for (size_t i = 0U; i < cnt; i++) {
+		uint8_t *data8 = (uint8_t *)ch[i].data;
+		size_t blen = ch[i].len;
+
+		while (blen != 0U) {
+			size_t cplen = MIN(blen, disk->ssize - bpos);
+
+			memcpy(data8, buf + bpos, cplen);
+			bpos += cplen;
+			blen -= cplen;
+			data8 += cplen;
+
+			if (bpos == sizeof(buf)) {
+				starts++;
+				rc = disk_access_read(disk->name, buf, starts,
+						      1U);
+				if (rc != 0) {
+					break;
+				}
+
+				bpos = 0U;
+			}
+		}
+	}
+
+	if (rc != 0) {
+		LOG_DBG("read failed at %x", starts * disk->ssize);
+	}
+end:
+	return rc;
+}
+
+static int sa_disk_prog(const struct storage_area *area, size_t start,
+			const struct storage_area_chunk *ch, size_t cnt)
+{
+	const struct storage_area_disk *disk =
+		CONTAINER_OF(area, struct storage_area_disk, area);
+	const size_t align = area->write_size;
+	const size_t spws = align / disk->ssize;
+	uint8_t buf[align];
+	size_t bpos = 0U;
+	size_t starts = start / disk->ssize;
+	int rc = sa_disk_valid(disk);
+
+	if (rc != 0) {
+		goto end;
+	}
+
+	starts += disk->start;
+	for (size_t i = 0U; i < cnt; i++) {
+		uint8_t *data8 = (uint8_t *)ch[i].data;
+		size_t blen = ch[i].len;
+
+		if (bpos != 0U) {
+			size_t cplen = MIN(blen, align - bpos);
+
+			memcpy(buf + bpos, data8, cplen);
+			bpos += cplen;
+			blen -= cplen;
+			data8 += cplen;
+
+			if (bpos == align) {
+				rc = disk_access_write(disk->name, buf, starts,
+						       spws);
+				if (rc != 0) {
+					break;
+				}
+
+				starts += spws;
+				bpos = 0U;
+			}
+		}
+
+		if (blen >= align) {
+			size_t wrlen = blen & ~(align - 1);
+			size_t wrs = wrlen / disk->ssize;
+
+			rc = disk_access_write(disk->name, data8, starts, wrs);
+			if (rc != 0) {
+				break;
+			}
+
+			blen -= wrlen;
+			data8 += wrlen;
+			starts += wrs;
+		}
+
+		if (blen > 0U) {
+			memcpy(buf, data8, blen);
+			bpos = blen;
+		}
+	}
+
+	if (rc != 0) {
+		LOG_DBG("prog failed at %x", starts * disk->ssize);
+	}
+end:
+	return rc;
+}
+
+static int sa_disk_erase(const struct storage_area *area, size_t start,
+			 size_t len)
+{
+	const struct storage_area_disk *disk =
+		CONTAINER_OF(area, struct storage_area_disk, area);
+	const size_t spws = area->erase_size / disk->ssize;
+	size_t starts = start * spws;
+	uint8_t buf[area->erase_size];
+	int rc = sa_disk_valid(disk);
+
+	if (rc != 0) {
+		goto end;
+	}
+
+	memset(buf, STORAGE_AREA_ERASEVALUE(area), sizeof(buf));
+	starts += disk->start;
+
+	for (size_t i = 0; i < len; i++) {
+		rc = disk_access_write(disk->name, buf, starts, spws);
+		if (rc != 0) {
+			break;
+		}
+
+		starts += spws;
+	}
+
+	if (rc != 0) {
+		LOG_DBG("prog failed at %x", starts * disk->ssize);
+	}
+end:
+	return rc;
+}
+
+static int sa_disk_ioctl(const struct storage_area *area,
+			 enum storage_area_ioctl_cmd cmd, void *data)
+{
+	const struct storage_area_disk *disk =
+		CONTAINER_OF(area, struct storage_area_disk, area);
+	int rc = sa_disk_valid(disk);
+
+	if (rc != 0) {
+		goto end;
+	}
+
+	rc = -ENOTSUP;
+	switch (cmd) {
+	default:
+		break;
+	}
+end:
+	return rc;
+}
+
+const struct storage_area_api storage_area_disk_api = {
+	.read = sa_disk_read,
+	.prog = sa_disk_prog,
+	.erase = sa_disk_erase,
+	.ioctl = sa_disk_ioctl,
+};

subsys/storage/storage_area/storage_area_ram.c
@@ -1,0 +1,130 @@
+/*
+ * Copyright (c) 2024 Laczen
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+#include <zephyr/storage/storage_area/storage_area_ram.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(storage_area_ram, CONFIG_STORAGE_AREA_LOG_LEVEL);
+
+static int sa_ram_read(const struct storage_area *area, size_t start,
+		       const struct storage_area_chunk *ch, size_t cnt)
+{
+	const struct storage_area_ram *ram =
+		CONTAINER_OF(area, struct storage_area_ram, area);
+	const uint8_t *rstart = (uint8_t *)ram->start;
+
+	for (size_t i = 0U; i < cnt; i++) {
+
+		memcpy(ch[i].data, rstart + start, ch[i].len);
+		start += ch[i].len;
+	}
+
+	return 0;
+}
+
+static int sa_ram_prog(const struct storage_area *area, size_t start,
+		       const struct storage_area_chunk *ch, size_t cnt)
+{
+	const struct storage_area_ram *ram =
+		CONTAINER_OF(area, struct storage_area_ram, area);
+	const size_t align = area->write_size;
+	uint8_t *rstart = (uint8_t *)ram->start;
+	uint8_t buf[align];
+	size_t bpos = 0U;
+	int rc = 0;
+
+	for (size_t i = 0U; i < cnt; i++) {
+		uint8_t *data8 = (uint8_t *)ch[i].data;
+		size_t blen = ch[i].len;
+
+		if (bpos != 0U) {
+			size_t cplen = MIN(blen, align - bpos);
+
+			memcpy(buf + bpos, data8, cplen);
+			bpos += cplen;
+			blen -= cplen;
+			data8 += cplen;
+
+			if (bpos == align) {
+				memcpy(rstart + start, buf, align);
+				start += align;
+				bpos = 0U;
+			}
+		}
+
+		if (blen >= align) {
+			size_t wrlen = blen & ~(align - 1);
+
+			memcpy(rstart + start, data8, wrlen);
+			blen -= wrlen;
+			data8 += wrlen;
+			start += wrlen;
+		}
+
+		if (blen > 0U) {
+			memcpy(buf, data8, blen);
+			bpos = blen;
+		}
+	}
+
+	return rc;
+}
+
+static int sa_ram_erase(const struct storage_area *area, size_t start,
+			size_t len)
+{
+	const struct storage_area_ram *ram =
+		CONTAINER_OF(area, struct storage_area_ram, area);
+
+	start *= area->erase_size;
+
+	for (size_t i = 0; i < len; i++) {
+		uint8_t *rstart = (uint8_t *)ram->start;
+		(void)memset(rstart + start, STORAGE_AREA_ERASEVALUE(area),
+			     area->erase_size);
+		start += area->erase_size;
+	}
+
+	return 0;
+}
+
+static int sa_ram_ioctl(const struct storage_area *area,
+			enum storage_area_ioctl_cmd cmd, void *data)
+{
+	const struct storage_area_ram *ram =
+		CONTAINER_OF(area, struct storage_area_ram, area);
+
+	int rc = -ENOTSUP;
+
+	switch (cmd) {
+	case SA_IOCTL_XIPADDRESS:
+		if (data == NULL) {
+			LOG_DBG("No return data supplied");
+			rc = -EINVAL;
+			break;
+		}
+
+		uintptr_t *xip_address = (uintptr_t *)data;
+		*xip_address = ram->start;
+		rc = 0;
+		break;
+
+	default:
+		break;
+	}
+
+	return rc;
+}
+
+const struct storage_area_api storage_area_ram_api = {
+	.read = sa_ram_read,
+	.prog = sa_ram_prog,
+	.erase = sa_ram_erase,
+	.ioctl = sa_ram_ioctl,
+};

anilmaguluri/zephyr_ble

subsys/storage/storage_area/storage_area.c
@@ -1,0 +1,132 @@
+/*
+ * Copyright (c) 2024 Laczen
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/storage/storage_area/storage_area.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(storage_area, CONFIG_STORAGE_AREA_LOG_LEVEL);
+
+static bool sa_range_valid(const struct storage_area *area, size_t start,
+			   size_t len)
+{
+	const size_t asize = area->erase_size * area->erase_blocks;
+
+	if ((asize < len) || ((asize - len) < start)) {
+		LOG_DBG("Invalid range");
+		return false;
+	}
+
+	return true;
+}
+
+static size_t sa_iovec_size(const struct storage_area_iovec *iovec,
+			    size_t iovcnt)
+{
+	size_t rv = 0U;
+
+	for (size_t i = 0U; i < iovcnt; i++) {
+		rv += iovec[i].len;
+	}
+
+	return rv;
+}
+
+int storage_area_readv(const struct storage_area *area, size_t start,
+		       const struct storage_area_iovec *iovec, size_t iovcnt)
+{
+	if ((area == NULL) || (area->api == NULL) || (area->api->readv == NULL)) {
+		return -ENOTSUP;
+	}
+
+	const size_t len = sa_iovec_size(iovec, iovcnt);
+
+	if (!sa_range_valid(area, start, len)) {
+		return -EINVAL;
+	}
+
+	return area->api->readv(area, start, iovec, iovcnt);
+}
+
+int storage_area_read(const struct storage_area *area, size_t start, void *data,
+		      size_t len)
+{
+	struct storage_area_iovec rd = {
+		.data = data,
+		.len = len,
+	};
+
+	return storage_area_readv(area, start, &rd, 1U);
+}
+
+int storage_area_writev(const struct storage_area *area, size_t start,
+			const struct storage_area_iovec *iovec, size_t iovcnt)
+{
+	if ((area == NULL) || (area->api == NULL) || (area->api->writev == NULL)) {
+		return -ENOTSUP;
+	}
+
+	const size_t len = sa_iovec_size(iovec, iovcnt);
+
+	if ((!sa_range_valid(area, start, len)) ||
+	    (((len & (area->write_size - 1)) != 0U))) {
+		return -EINVAL;
+	}
+
+	if (STORAGE_AREA_HAS_PROPERTY(area, SA_PROP_READONLY)) {
+		LOG_DBG("prog not supported (read-only)");
+		return -EROFS;
+	}
+
+	return area->api->writev(area, start, iovec, iovcnt);
+}
+
+int storage_area_write(const struct storage_area *area, size_t start,
+		       const void *data, size_t len)
+{
+	struct storage_area_iovec wr = {
+		.data = (void *)data,
+		.len = len,
+	};
+
+	return storage_area_writev(area, start, &wr, 1U);
+}
+
+int storage_area_erase(const struct storage_area *area, size_t start,
+		       size_t bcnt)
+{
+	if ((area == NULL) || (area->api == NULL) ||
+	    (area->api->erase == NULL)) {
+		return -ENOTSUP;
+	}
+
+	const size_t ablocks = area->erase_blocks;
+
+	if ((ablocks < bcnt) || ((ablocks - bcnt) < start)) {
+		LOG_DBG("invalid range");
+		return -EINVAL;
+	}
+
+	if (STORAGE_AREA_HAS_PROPERTY(area, SA_PROP_READONLY)) {
+		LOG_DBG("erase not supported (read-only)");
+		return -EROFS;
+	}
+
+	return area->api->erase(area, start, bcnt);
+}
+
+int storage_area_ioctl(const struct storage_area *area,
+		       enum storage_area_ioctl_cmd cmd, void *data)
+{
+	if ((area == NULL) || (area->api == NULL) ||
+	    (area->api->ioctl == NULL)) {
+		return -ENOTSUP;
+	}
+
+	return area->api->ioctl(area, cmd, data);
+}

subsys/storage/storage_area/storage_area_disk.c
@@ -1,0 +1,250 @@
+/*
+ * Copyright (c) 2024 Laczen
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string.h>
+#include <errno.h>
+#include <zephyr/storage/storage_area/storage_area_disk.h>
+#include <zephyr/storage/disk_access.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(storage_area_disk, CONFIG_STORAGE_AREA_LOG_LEVEL);
+
+static int sa_disk_valid(const struct storage_area_disk *disk)
+{
+	int rc = disk_access_init(disk->name);
+
+	if (rc != 0) {
+		return rc;
+	}
+
+	if (IS_ENABLED(CONFIG_STORAGE_AREA_VERIFY)) {
+		size_t scount;
+		size_t ssize;
+
+		rc = disk_access_ioctl(disk->name, DISK_IOCTL_GET_SECTOR_COUNT,
+				       &scount);
+		if (rc != 0) {
+			LOG_DBG("Unable to get disk sector count");
+			return -EINVAL;
+		}
+
+		rc = disk_access_ioctl(disk->name, DISK_IOCTL_GET_SECTOR_SIZE,
+				       &ssize);
+		if (rc != 0) {
+			LOG_DBG("Unable to get disk sector size");
+			return -EINVAL;
+		}
+
+		if (disk->ssize != ssize) {
+			LOG_DBG("Bad disk sector size");
+			return -EINVAL;
+		}
+
+		const struct storage_area *area = &disk->area;
+		const size_t asz = area->erase_blocks * area->erase_size;
+		const size_t esz = scount * ssize;
+
+		if (esz < ((disk->start * ssize) + asz)) {
+			LOG_DBG("Bad area size");
+			return -EINVAL;
+		}
+
+		if ((area->write_size & (ssize - 1)) != 0U) {
+			LOG_DBG("Bad area write size");
+			return -EINVAL;
+		}
+
+		if ((area->erase_size & (ssize - 1)) != 0U) {
+			LOG_DBG("Bad area erase size");
+			return -EINVAL;
+		}
+	}
+
+	return 0;
+}
+
+static int sa_disk_readv(const struct storage_area *area, size_t start,
+			 const struct storage_area_iovec *iovec, size_t iovcnt)
+{
+	const struct storage_area_disk *disk =
+		CONTAINER_OF(area, struct storage_area_disk, area);
+	size_t starts = start / disk->ssize;
+	size_t bpos = start % disk->ssize;
+	uint8_t buf[disk->ssize];
+	int rc = sa_disk_valid(disk);
+
+	if (rc != 0) {
+		goto end;
+	}
+
+	starts += disk->start;
+	rc = disk_access_read(disk->name, buf, starts, 1U);
+	if (rc != 0) {
+		goto end;
+	}
+
+	for (size_t i = 0U; i < iovcnt; i++) {
+		uint8_t *data8 = (uint8_t *)iovec[i].data;
+		size_t blen = iovec[i].len;
+
+		while (blen != 0U) {
+			size_t cplen = MIN(blen, disk->ssize - bpos);
+
+			memcpy(data8, buf + bpos, cplen);
+			bpos += cplen;
+			blen -= cplen;
+			data8 += cplen;
+
+			if (bpos == sizeof(buf)) {
+				starts++;
+				rc = disk_access_read(disk->name, buf, starts,
+						      1U);
+				if (rc != 0) {
+					break;
+				}
+
+				bpos = 0U;
+			}
+		}
+	}
+
+	if (rc != 0) {
+		LOG_DBG("read failed at %x",
+			(starts - disk->start) * disk->ssize);
+	}
+end:
+	return rc;
+}
+
+static int sa_disk_writev(const struct storage_area *area, size_t start,
+			  const struct storage_area_iovec *iovec, size_t iovcnt)
+{
+	const struct storage_area_disk *disk =
+		CONTAINER_OF(area, struct storage_area_disk, area);
+	const size_t align = area->write_size;
+	const size_t spws = align / disk->ssize;
+	uint8_t buf[align];
+	size_t bpos = 0U;
+	size_t starts = start / disk->ssize;
+	int rc = sa_disk_valid(disk);
+
+	if (rc != 0) {
+		goto end;
+	}
+
+	starts += disk->start;
+	for (size_t i = 0U; i < iovcnt; i++) {
+		uint8_t *data8 = (uint8_t *)iovec[i].data;
+		size_t blen = iovec[i].len;
+
+		if (bpos != 0U) {
+			size_t cplen = MIN(blen, align - bpos);
+
+			memcpy(buf + bpos, data8, cplen);
+			bpos += cplen;
+			blen -= cplen;
+			data8 += cplen;
+
+			if (bpos == align) {
+				rc = disk_access_write(disk->name, buf, starts,
+						       spws);
+				if (rc != 0) {
+					break;
+				}
+
+				starts += spws;
+				bpos = 0U;
+			}
+		}
+
+		if (blen >= align) {
+			size_t wrlen = blen & ~(align - 1);
+			size_t wrs = wrlen / disk->ssize;
+
+			rc = disk_access_write(disk->name, data8, starts, wrs);
+			if (rc != 0) {
+				break;
+			}
+
+			blen -= wrlen;
+			data8 += wrlen;
+			starts += wrs;
+		}
+
+		if (blen > 0U) {
+			memcpy(buf, data8, blen);
+			bpos = blen;
+		}
+	}
+
+	if (rc != 0) {
+		LOG_DBG("write failed at %x",
+			(starts - disk->start) * disk->ssize);
+	}
+end:
+	return rc;
+}
+
+static int sa_disk_erase(const struct storage_area *area, size_t start,
+			 size_t len)
+{
+	const struct storage_area_disk *disk =
+		CONTAINER_OF(area, struct storage_area_disk, area);
+	const size_t spws = area->erase_size / disk->ssize;
+	size_t starts = start * spws;
+	uint8_t buf[area->erase_size];
+	int rc = sa_disk_valid(disk);
+
+	if (rc != 0) {
+		goto end;
+	}
+
+	memset(buf, STORAGE_AREA_ERASEVALUE(area), sizeof(buf));
+	starts += disk->start;
+
+	for (size_t i = 0; i < len; i++) {
+		rc = disk_access_write(disk->name, buf, starts, spws);
+		if (rc != 0) {
+			break;
+		}
+
+		starts += spws;
+	}
+
+	if (rc != 0) {
+		LOG_DBG("write failed at %x",
+			(starts - disk->start) * disk->ssize);
+	}
+end:
+	return rc;
+}
+
+static int sa_disk_ioctl(const struct storage_area *area,
+			 enum storage_area_ioctl_cmd cmd, void *data)
+{
+	const struct storage_area_disk *disk =
+		CONTAINER_OF(area, struct storage_area_disk, area);
+	int rc = sa_disk_valid(disk);
+
+	if (rc != 0) {
+		goto end;
+	}
+
+	rc = -ENOTSUP;
+	switch (cmd) {
+	default:
+		break;
+	}
+end:
+	return rc;
+}
+
+const struct storage_area_api storage_area_disk_api = {
+	.readv = sa_disk_readv,
+	.writev = sa_disk_writev,
+	.erase = sa_disk_erase,
+	.ioctl = sa_disk_ioctl,
+};

subsys/storage/storage_area/storage_area_ram.c
@@ -1,0 +1,130 @@
+/*
+ * Copyright (c) 2024 Laczen
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+#include <zephyr/storage/storage_area/storage_area_ram.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(storage_area_ram, CONFIG_STORAGE_AREA_LOG_LEVEL);
+
+static int sa_ram_readv(const struct storage_area *area, size_t start,
+			const struct storage_area_iovec *iovec, size_t iovcnt)
+{
+	const struct storage_area_ram *ram =
+		CONTAINER_OF(area, struct storage_area_ram, area);
+	const uint8_t *rstart = (uint8_t *)ram->start;
+
+	for (size_t i = 0U; i < iovcnt; i++) {
+
+		memcpy(iovec[i].data, rstart + start, iovec[i].len);
+		start += iovec[i].len;
+	}
+
+	return 0;
+}
+
+static int sa_ram_writev(const struct storage_area *area, size_t start,
+			 const struct storage_area_iovec *iovec, size_t iovcnt)
+{
+	const struct storage_area_ram *ram =
+		CONTAINER_OF(area, struct storage_area_ram, area);
+	const size_t align = area->write_size;
+	uint8_t *rstart = (uint8_t *)ram->start;
+	uint8_t buf[align];
+	size_t bpos = 0U;
+	int rc = 0;
+
+	for (size_t i = 0U; i < iovcnt; i++) {
+		uint8_t *data8 = (uint8_t *)iovec[i].data;
+		size_t blen = iovec[i].len;
+
+		if (bpos != 0U) {
+			size_t cplen = MIN(blen, align - bpos);
+
+			memcpy(buf + bpos, data8, cplen);
+			bpos += cplen;
+			blen -= cplen;
+			data8 += cplen;
+
+			if (bpos == align) {
+				memcpy(rstart + start, buf, align);
+				start += align;
+				bpos = 0U;
+			}
+		}
+
+		if (blen >= align) {
+			size_t wrlen = blen & ~(align - 1);
+
+			memcpy(rstart + start, data8, wrlen);
+			blen -= wrlen;
+			data8 += wrlen;
+			start += wrlen;
+		}
+
+		if (blen > 0U) {
+			memcpy(buf, data8, blen);
+			bpos = blen;
+		}
+	}
+
+	return rc;
+}
+
+static int sa_ram_erase(const struct storage_area *area, size_t start,
+			size_t len)
+{
+	const struct storage_area_ram *ram =
+		CONTAINER_OF(area, struct storage_area_ram, area);
+
+	start *= area->erase_size;
+
+	for (size_t i = 0; i < len; i++) {
+		uint8_t *rstart = (uint8_t *)ram->start;
+		(void)memset(rstart + start, STORAGE_AREA_ERASEVALUE(area),
+			     area->erase_size);
+		start += area->erase_size;
+	}
+
+	return 0;
+}
+
+static int sa_ram_ioctl(const struct storage_area *area,
+			enum storage_area_ioctl_cmd cmd, void *data)
+{
+	const struct storage_area_ram *ram =
+		CONTAINER_OF(area, struct storage_area_ram, area);
+
+	int rc = -ENOTSUP;
+
+	switch (cmd) {
+	case SA_IOCTL_XIPADDRESS:
+		if (data == NULL) {
+			LOG_DBG("No return data supplied");
+			rc = -EINVAL;
+			break;
+		}
+
+		uintptr_t *xip_address = (uintptr_t *)data;
+		*xip_address = ram->start;
+		rc = 0;
+		break;
+
+	default:
+		break;
+	}
+
+	return rc;
+}
+
+const struct storage_area_api storage_area_ram_api = {
+	.readv = sa_ram_readv,
+	.writev = sa_ram_writev,
+	.erase = sa_ram_erase,
+	.ioctl = sa_ram_ioctl,
+};