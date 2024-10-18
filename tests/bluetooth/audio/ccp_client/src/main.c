Johnsonyu0926/zephyr

Bluetooth: CCP: Initial CCP Client implemenation
Added initial CCP client implementation that simply
does discovery of TBS on a remote CCP server.

Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

tests/bluetooth/audio/ccp_client/src/main.c
@@ -1,0 +1,202 @@
+/* main.c - Application main entry point */
+
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <zephyr/autoconf.h>
+#include <zephyr/bluetooth/audio/ccp.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/hci_types.h>
+#include <zephyr/fff.h>
+#include <zephyr/sys/util_macro.h>
+#include <zephyr/ztest_test.h>
+#include <zephyr/ztest_assert.h>
+
+#include "conn.h"
+#include "ccp_client.h"
+#include "expects_util.h"
+
+DEFINE_FFF_GLOBALS;
+
+struct ccp_client_test_suite_fixture {
+	/** Need 1 additional bearer than the max to trigger some corner cases */
+	struct bt_ccp_client_bearer *bearers[CONFIG_BT_CCP_CLIENT_BEARER_COUNT + 1];
+	struct bt_ccp_client *client_inst;
+	struct bt_conn conn;
+};
+
+static void mock_init_rule_before(const struct ztest_unit_test *test, void *fixture)
+{
+	mock_ccp_client_init();
+}
+
+static void mock_destroy_rule_after(const struct ztest_unit_test *test, void *fixture)
+{
+	mock_ccp_client_cleanup();
+}
+
+ZTEST_RULE(mock_rule, mock_init_rule_before, mock_destroy_rule_after);
+
+static void test_conn_init(struct bt_conn *conn)
+{
+	conn->index = 0;
+	conn->info.type = BT_CONN_TYPE_LE;
+	conn->info.role = BT_CONN_ROLE_CENTRAL;
+	conn->info.state = BT_CONN_STATE_CONNECTED;
+	conn->info.security.level = BT_SECURITY_L2;
+	conn->info.security.enc_key_size = BT_ENC_KEY_SIZE_MAX;
+	conn->info.security.flags = BT_SECURITY_FLAG_OOB | BT_SECURITY_FLAG_SC;
+
+	mock_bt_conn_connected(conn, BT_HCI_ERR_SUCCESS);
+}
+
+static void *ccp_client_test_suite_setup(void)
+{
+	struct ccp_client_test_suite_fixture *fixture;
+
+	fixture = malloc(sizeof(*fixture));
+	zassert_not_null(fixture);
+
+	return fixture;
+}
+
+static void ccp_client_test_suite_before(void *f)
+{
+	struct ccp_client_test_suite_fixture *fixture = f;
+
+	memset(fixture, 0, sizeof(*fixture));
+	test_conn_init(&fixture->conn);
+}
+
+static void ccp_client_test_suite_after(void *f)
+{
+	struct ccp_client_test_suite_fixture *fixture = f;
+
+	(void)bt_ccp_client_unregister_cb(&mock_ccp_client_cb);
+	mock_bt_conn_disconnected(&fixture->conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
+}
+
+static void ccp_client_test_suite_teardown(void *f)
+{
+	free(f);
+}
+
+ZTEST_SUITE(ccp_client_test_suite, NULL, ccp_client_test_suite_setup, ccp_client_test_suite_before,
+	    ccp_client_test_suite_after, ccp_client_test_suite_teardown);
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_register_cb)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_register_cb_inval_param_null)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(NULL);
+	zassert_equal(-EINVAL, err, "Unexpected return value %d", err);
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_register_cb_inval_double_register)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(-EEXIST, err, "Unexpected return value %d", err);
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_unregister_cb)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	err = bt_ccp_client_unregister_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_unregister_cb_inval_param_null)
+{
+	int err;
+
+	err = bt_ccp_client_unregister_cb(NULL);
+	zassert_equal(-EINVAL, err, "Unexpected return value %d", err);
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_unregister_cb_inval_double_unregister)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	err = bt_ccp_client_unregister_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	err = bt_ccp_client_unregister_cb(&mock_ccp_client_cb);
+	zassert_equal(-EALREADY, err, "Unexpected return value %d", err);
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_discover)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	err = bt_ccp_client_discover(&fixture->conn, &fixture->client_inst);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	/* Validate that we got the callback with valid values */
+	zexpect_call_count("bt_ccp_client_cb.discover", 1,
+			   mock_ccp_client_discover_cb_fake.call_count);
+	zassert_not_null(mock_ccp_client_discover_cb_fake.arg0_history[0]);
+	zassert_equal(0, mock_ccp_client_discover_cb_fake.arg1_history[0]);
+	zassert_not_null(mock_ccp_client_discover_cb_fake.arg2_history[0]);
+
+#if defined(CONFIG_BT_TBS_CLIENT_GTBS)
+	zassert_not_null(mock_ccp_client_discover_cb_fake.arg2_history[0]->gtbs_bearer);
+#endif /* CONFIG_BT_TBS_CLIENT_GTBS */
+
+#if defined(CONFIG_BT_TBS_CLIENT_TBS)
+	zassert_equal(CONFIG_BT_TBS_CLIENT_MAX_TBS_INSTANCES,
+		      mock_ccp_client_discover_cb_fake.arg2_history[0]->tbs_count);
+	zassert_not_null(mock_ccp_client_discover_cb_fake.arg2_history[0]->tbs_bearers);
+#endif /* CONFIG_BT_TBS_CLIENT_TBS */
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_discover_inval_param_null_conn)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	err = bt_ccp_client_discover(NULL, &fixture->client_inst);
+	zassert_equal(-EINVAL, err, "Unexpected return value %d", err);
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_discover_inval_param_null_client)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	err = bt_ccp_client_discover(&fixture->conn, NULL);
+	zassert_equal(-EINVAL, err, "Unexpected return value %d", err);
+}

tests/bsim/bluetooth/audio_samples/ccp/client/src/test_main.c
@@ -1,0 +1,69 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "bs_types.h"
+#include "bs_tracing.h"
+#include "bs_utils.h"
+#include "bstests.h"
+
+#define WAIT_TIME 10 /* Seconds */
+
+extern enum bst_result_t bst_result;
+
+#define FAIL(...)                                                                                  \
+	do {                                                                                       \
+		bst_result = Failed;                                                               \
+		bs_trace_error_time_line(__VA_ARGS__);                                             \
+	} while (0)
+
+#define PASS(...)                                                                                  \
+	do {                                                                                       \
+		bst_result = Passed;                                                               \
+		bs_trace_info_time(1, __VA_ARGS__);                                                \
+	} while (0)
+
+static void test_ccp_client_sample_init(void)
+{
+	bst_ticker_set_next_tick_absolute(WAIT_TIME * 1e6);
+	bst_result = In_progress;
+}
+
+static void test_ccp_client_sample_tick(bs_time_t HW_device_time)
+{
+	extern struct bt_ccp_client *client_inst;
+
+	/* If discovery was a success then client_inst is non-NULL - Use as pass criteria */
+	if (client_inst == NULL) {
+		FAIL("CCP client sample FAILED (Did not pass after %i seconds)\n", WAIT_TIME);
+	} else {
+		PASS("CCP client sample PASSED\n");
+	}
+}
+
+static const struct bst_test_instance test_sample[] = {
+	{
+		.test_id = "ccp_client",
+		.test_descr = "Test based on the CCP client sample. "
+			      "It expects to be connected to a compatible CCP server, "
+			      "waits for " STR(WAIT_TIME) " seconds, and checks how "
+			      "many audio packets have been received correctly",
+		.test_post_init_f = test_ccp_client_sample_init,
+		.test_tick_f = test_ccp_client_sample_tick,
+	},
+	BSTEST_END_MARKER,
+};
+
+static struct bst_test_list *test_ccp_client_sample_install(struct bst_test_list *tests)
+{
+	tests = bst_add_tests(tests, test_sample);
+	return tests;
+}
+
+bst_test_install_t test_installers[] = {test_ccp_client_sample_install, NULL};

anilmaguluri/zephyr_ble

tests/bluetooth/audio/ccp_client/src/main.c
@@ -1,0 +1,202 @@
+/* main.c - Application main entry point */
+
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <zephyr/autoconf.h>
+#include <zephyr/bluetooth/audio/ccp.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/hci_types.h>
+#include <zephyr/fff.h>
+#include <zephyr/sys/util_macro.h>
+#include <zephyr/ztest_test.h>
+#include <zephyr/ztest_assert.h>
+
+#include "conn.h"
+#include "ccp_client.h"
+#include "expects_util.h"
+
+DEFINE_FFF_GLOBALS;
+
+struct ccp_client_test_suite_fixture {
+	/** Need 1 additional bearer than the max to trigger some corner cases */
+	struct bt_ccp_client_bearer *bearers[CONFIG_BT_CCP_CLIENT_BEARER_COUNT + 1];
+	struct bt_ccp_client *client;
+	struct bt_conn conn;
+};
+
+static void mock_init_rule_before(const struct ztest_unit_test *test, void *fixture)
+{
+	mock_ccp_client_init();
+}
+
+static void mock_destroy_rule_after(const struct ztest_unit_test *test, void *fixture)
+{
+	mock_ccp_client_cleanup();
+}
+
+ZTEST_RULE(mock_rule, mock_init_rule_before, mock_destroy_rule_after);
+
+static void test_conn_init(struct bt_conn *conn)
+{
+	conn->index = 0;
+	conn->info.type = BT_CONN_TYPE_LE;
+	conn->info.role = BT_CONN_ROLE_CENTRAL;
+	conn->info.state = BT_CONN_STATE_CONNECTED;
+	conn->info.security.level = BT_SECURITY_L2;
+	conn->info.security.enc_key_size = BT_ENC_KEY_SIZE_MAX;
+	conn->info.security.flags = BT_SECURITY_FLAG_OOB | BT_SECURITY_FLAG_SC;
+
+	mock_bt_conn_connected(conn, BT_HCI_ERR_SUCCESS);
+}
+
+static void *ccp_client_test_suite_setup(void)
+{
+	struct ccp_client_test_suite_fixture *fixture;
+
+	fixture = malloc(sizeof(*fixture));
+	zassert_not_null(fixture);
+
+	return fixture;
+}
+
+static void ccp_client_test_suite_before(void *f)
+{
+	struct ccp_client_test_suite_fixture *fixture = f;
+
+	memset(fixture, 0, sizeof(*fixture));
+	test_conn_init(&fixture->conn);
+}
+
+static void ccp_client_test_suite_after(void *f)
+{
+	struct ccp_client_test_suite_fixture *fixture = f;
+
+	(void)bt_ccp_client_unregister_cb(&mock_ccp_client_cb);
+	mock_bt_conn_disconnected(&fixture->conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
+}
+
+static void ccp_client_test_suite_teardown(void *f)
+{
+	free(f);
+}
+
+ZTEST_SUITE(ccp_client_test_suite, NULL, ccp_client_test_suite_setup, ccp_client_test_suite_before,
+	    ccp_client_test_suite_after, ccp_client_test_suite_teardown);
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_register_cb)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_register_cb_inval_param_null)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(NULL);
+	zassert_equal(-EINVAL, err, "Unexpected return value %d", err);
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_register_cb_inval_double_register)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(-EEXIST, err, "Unexpected return value %d", err);
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_unregister_cb)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	err = bt_ccp_client_unregister_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_unregister_cb_inval_param_null)
+{
+	int err;
+
+	err = bt_ccp_client_unregister_cb(NULL);
+	zassert_equal(-EINVAL, err, "Unexpected return value %d", err);
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_unregister_cb_inval_double_unregister)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	err = bt_ccp_client_unregister_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	err = bt_ccp_client_unregister_cb(&mock_ccp_client_cb);
+	zassert_equal(-EALREADY, err, "Unexpected return value %d", err);
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_discover)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	err = bt_ccp_client_discover(&fixture->conn, &fixture->client);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	/* Validate that we got the callback with valid values */
+	zexpect_call_count("bt_ccp_client_cb.discover", 1,
+			   mock_ccp_client_discover_cb_fake.call_count);
+	zassert_not_null(mock_ccp_client_discover_cb_fake.arg0_history[0]);
+	zassert_equal(0, mock_ccp_client_discover_cb_fake.arg1_history[0]);
+	zassert_not_null(mock_ccp_client_discover_cb_fake.arg2_history[0]);
+
+#if defined(CONFIG_BT_TBS_CLIENT_GTBS)
+	zassert_not_null(mock_ccp_client_discover_cb_fake.arg2_history[0]->gtbs_bearer);
+#endif /* CONFIG_BT_TBS_CLIENT_GTBS */
+
+#if defined(CONFIG_BT_TBS_CLIENT_TBS)
+	zassert_equal(CONFIG_BT_TBS_CLIENT_MAX_TBS_INSTANCES,
+		      mock_ccp_client_discover_cb_fake.arg2_history[0]->tbs_count);
+	zassert_not_null(mock_ccp_client_discover_cb_fake.arg2_history[0]->tbs_bearers);
+#endif /* CONFIG_BT_TBS_CLIENT_TBS */
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_discover_inval_param_null_conn)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	err = bt_ccp_client_discover(NULL, &fixture->client);
+	zassert_equal(-EINVAL, err, "Unexpected return value %d", err);
+}
+
+static ZTEST_F(ccp_client_test_suite, test_ccp_client_discover_inval_param_null_client)
+{
+	int err;
+
+	err = bt_ccp_client_register_cb(&mock_ccp_client_cb);
+	zassert_equal(0, err, "Unexpected return value %d", err);
+
+	err = bt_ccp_client_discover(&fixture->conn, NULL);
+	zassert_equal(-EINVAL, err, "Unexpected return value %d", err);
+}

tests/bsim/bluetooth/audio_samples/ccp/client/src/test_main.c
@@ -1,0 +1,69 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "bs_types.h"
+#include "bs_tracing.h"
+#include "bs_utils.h"
+#include "bstests.h"
+
+#define WAIT_TIME 10 /* Seconds */
+
+extern enum bst_result_t bst_result;
+
+#define FAIL(...)                                                                                  \
+	do {                                                                                       \
+		bst_result = Failed;                                                               \
+		bs_trace_error_time_line(__VA_ARGS__);                                             \
+	} while (0)
+
+#define PASS(...)                                                                                  \
+	do {                                                                                       \
+		bst_result = Passed;                                                               \
+		bs_trace_info_time(1, __VA_ARGS__);                                                \
+	} while (0)
+
+static void test_ccp_client_sample_init(void)
+{
+	bst_ticker_set_next_tick_absolute(WAIT_TIME * 1e6);
+	bst_result = In_progress;
+}
+
+static void test_ccp_client_sample_tick(bs_time_t HW_device_time)
+{
+	extern struct bt_ccp_client *client;
+
+	/* If discovery was a success then client is non-NULL - Use as pass criteria */
+	if (client == NULL) {
+		FAIL("CCP client sample FAILED (Did not pass after %i seconds)\n", WAIT_TIME);
+	} else {
+		PASS("CCP client sample PASSED\n");
+	}
+}
+
+static const struct bst_test_instance test_sample[] = {
+	{
+		.test_id = "ccp_client",
+		.test_descr = "Test based on the CCP client sample. "
+			      "It expects to be connected to a compatible CCP server, "
+			      "waits for " STR(WAIT_TIME) " seconds, and checks how "
+			      "many audio packets have been received correctly",
+		.test_post_init_f = test_ccp_client_sample_init,
+		.test_tick_f = test_ccp_client_sample_tick,
+	},
+	BSTEST_END_MARKER,
+};
+
+static struct bst_test_list *test_ccp_client_sample_install(struct bst_test_list *tests)
+{
+	tests = bst_add_tests(tests, test_sample);
+	return tests;
+}
+
+bst_test_install_t test_installers[] = {test_ccp_client_sample_install, NULL};