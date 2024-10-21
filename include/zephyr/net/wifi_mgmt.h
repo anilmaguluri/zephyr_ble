Johnsonyu0926/zephyr

modules: hostap: add 11k/v/r roaming support
Added new ops and events in glue layer to support roaming.
Added new flag WIFI_NM_WPA_SUPPLICANT_ROAMING to control roaming
feature.

Signed-off-by: Hui Bai <hui.bai@nxp.com>

include/zephyr/net/wifi_mgmt.h
@@ -112,6 +112,12 @@ enum net_request_wifi_cmd {
 	NET_REQUEST_WIFI_CMD_RTS_THRESHOLD_CONFIG,
 	/** WPS config */
 	NET_REQUEST_WIFI_CMD_WPS_CONFIG,
+	/** Start roaming */
+	NET_REQUEST_WIFI_CMD_START_ROAMING,
+	/** Neighbor report complete */
+	NET_REQUEST_WIFI_CMD_NEIGHBOR_REP_COMPLETE,
+	/** Specific scan */
+	NET_REQUEST_WIFI_CMD_SPEC_SCAN,
 	/** @cond INTERNAL_HIDDEN */
 	NET_REQUEST_WIFI_CMD_MAX
 /** @endcond */
@@ -272,6 +278,16 @@ NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_RTS_THRESHOLD_CONFIG);
 
 NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_WPS_CONFIG);
 
+#define NET_REQUEST_WIFI_START_ROAMING				\
+	(_NET_WIFI_BASE | NET_REQUEST_WIFI_CMD_START_ROAMING)
+
+NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_START_ROAMING);
+
+#define NET_REQUEST_WIFI_NEIGHBOR_REP_COMPLETE			\
+	(_NET_WIFI_BASE | NET_REQUEST_WIFI_CMD_NEIGHBOR_REP_COMPLETE)
+
+NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_NEIGHBOR_REP_COMPLETE);
+
 /** @brief Wi-Fi management events */
 enum net_event_wifi_cmd {
 	/** Scan results available */
@@ -294,6 +310,12 @@ enum net_event_wifi_cmd {
 	NET_EVENT_WIFI_CMD_RAW_SCAN_RESULT,
 	/** Disconnect complete */
 	NET_EVENT_WIFI_CMD_DISCONNECT_COMPLETE,
+	/** Signal change event */
+	NET_EVENT_WIFI_CMD_SIGNAL_CHANGE,
+	/** Neighbor Report */
+	NET_EVENT_WIFI_CMD_NEIGHBOR_REP_RECEIVED,
+	/** Neighbor Report complete */
+	NET_EVENT_WIFI_CMD_NEIGHBOR_REP_COMPLETE,
 	/** AP mode enable result */
 	NET_EVENT_WIFI_CMD_AP_ENABLE_RESULT,
 	/** AP mode disable result */
@@ -341,6 +363,14 @@ enum net_event_wifi_cmd {
 /** Event emitted Wi-Fi disconnect is completed */
 #define NET_EVENT_WIFI_DISCONNECT_COMPLETE			\
 	(_NET_WIFI_EVENT | NET_EVENT_WIFI_CMD_DISCONNECT_COMPLETE)
+
+/** Event signal change of connected AP */
+#define NET_EVENT_WIFI_SIGNAL_CHANGE				\
+	(_NET_WIFI_EVENT | NET_EVENT_WIFI_CMD_SIGNAL_CHANGE)
+
+/** Event Neighbor Report Completed */
+#define NET_EVENT_WIFI_NEIGHBOR_REP_COMP			\
+	(_NET_WIFI_EVENT | NET_EVENT_WIFI_CMD_NEIGHBOR_REP_COMPLETE)
 
 /** Event emitted for Wi-Fi access point enable result */
 #define NET_EVENT_WIFI_AP_ENABLE_RESULT				\
@@ -1347,6 +1377,21 @@ struct wifi_mgmt_ops {
 	 * @return 0 if ok, < 0 if error
 	 */
 	int (*wps_config)(const struct device *dev, struct wifi_wps_config_params *params);
+	/** Trigger specific scan
+	 *
+	 * @param dev Pointer to the device structure for the driver instance
+	 * @param params Scan parameters
+	 *
+	 * @return 0 if ok, < 0 if error
+	 */
+	int (*spec_scan)(const struct device *dev, struct wifi_scan_params *params);
+	/** Start 11r roaming
+	 *
+	 * @param dev Pointer to the device structure for the driver instance
+	 *
+	 * @return 0 if ok, < 0 if error
+	 */
+	int (*start_11r_roaming)(const struct device *dev);
 };
 
 /** Wi-Fi management offload API */
@@ -1437,6 +1482,17 @@ void wifi_mgmt_raise_raw_scan_result_event(struct net_if *iface,
  * @param status Disconnect complete status
  */
 void wifi_mgmt_raise_disconnect_complete_event(struct net_if *iface, int status);
+
+#ifdef CONFIG_WIFI_NM_WPA_SUPPLICANT_ROAMING
+/** Wi-Fi management neighbor reports event
+ *
+ * @param iface Network interface
+ * @param inbuf Input buffer of neighbor reports
+ * @param buf_len Lenghth of input buffer
+ */
+void wifi_mgmt_raise_neighbor_rep_recv_event(struct net_if *iface,
+					     char *inbuf, size_t buf_len);
+#endif
 
 /** Wi-Fi management AP mode enable result event
  *

modules/hostap/src/supp_api.h
@@ -118,6 +118,25 @@ int supplicant_11k_cfg(const struct device *dev, struct wifi_11k_params *params)
  */
 int supplicant_11k_neighbor_request(const struct device *dev, struct wifi_11k_params *params);
 
+#ifdef CONFIG_WIFI_NM_WPA_SUPPLICANT_ROAMING
+/** Send specific scan request
+ *
+ * @param dev Pointer to the device structure for the driver instance.
+ * @param params Scan parameters
+ *
+ * @return 0 if ok, < 0 if error
+ */
+int supplicant_spec_scan(const struct device *dev, struct wifi_scan_params *params);
+
+/** Send 11r roaming request
+ *
+ * @param dev Pointer to the device structure for the driver instance.
+ *
+ * @return 0 if ok, < 0 if error
+ */
+int supplicant_11r_roaming(const struct device *dev);
+#endif
+
 /**
  * @brief Set Wi-Fi power save configuration
  *

anilmaguluri/zephyr_ble

include/zephyr/net/wifi_mgmt.h
@@ -112,6 +112,12 @@ enum net_request_wifi_cmd {
 	NET_REQUEST_WIFI_CMD_RTS_THRESHOLD_CONFIG,
 	/** WPS config */
 	NET_REQUEST_WIFI_CMD_WPS_CONFIG,
+	/** Start roaming */
+	NET_REQUEST_WIFI_CMD_START_ROAMING,
+	/** Neighbor report complete */
+	NET_REQUEST_WIFI_CMD_NEIGHBOR_REP_COMPLETE,
+	/** Specific scan */
+	NET_REQUEST_WIFI_CMD_CANDIDATE_SCAN,
 	/** @cond INTERNAL_HIDDEN */
 	NET_REQUEST_WIFI_CMD_MAX
 /** @endcond */
@@ -272,6 +278,16 @@ NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_RTS_THRESHOLD_CONFIG);
 
 NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_WPS_CONFIG);
 
+#define NET_REQUEST_WIFI_START_ROAMING				\
+	(_NET_WIFI_BASE | NET_REQUEST_WIFI_CMD_START_ROAMING)
+
+NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_START_ROAMING);
+
+#define NET_REQUEST_WIFI_NEIGHBOR_REP_COMPLETE			\
+	(_NET_WIFI_BASE | NET_REQUEST_WIFI_CMD_NEIGHBOR_REP_COMPLETE)
+
+NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_NEIGHBOR_REP_COMPLETE);
+
 /** @brief Wi-Fi management events */
 enum net_event_wifi_cmd {
 	/** Scan results available */
@@ -294,6 +310,12 @@ enum net_event_wifi_cmd {
 	NET_EVENT_WIFI_CMD_RAW_SCAN_RESULT,
 	/** Disconnect complete */
 	NET_EVENT_WIFI_CMD_DISCONNECT_COMPLETE,
+	/** Signal change event */
+	NET_EVENT_WIFI_CMD_SIGNAL_CHANGE,
+	/** Neighbor Report */
+	NET_EVENT_WIFI_CMD_NEIGHBOR_REP_RECEIVED,
+	/** Neighbor Report complete */
+	NET_EVENT_WIFI_CMD_NEIGHBOR_REP_COMPLETE,
 	/** AP mode enable result */
 	NET_EVENT_WIFI_CMD_AP_ENABLE_RESULT,
 	/** AP mode disable result */
@@ -341,6 +363,14 @@ enum net_event_wifi_cmd {
 /** Event emitted Wi-Fi disconnect is completed */
 #define NET_EVENT_WIFI_DISCONNECT_COMPLETE			\
 	(_NET_WIFI_EVENT | NET_EVENT_WIFI_CMD_DISCONNECT_COMPLETE)
+
+/** Event signal change of connected AP */
+#define NET_EVENT_WIFI_SIGNAL_CHANGE				\
+	(_NET_WIFI_EVENT | NET_EVENT_WIFI_CMD_SIGNAL_CHANGE)
+
+/** Event Neighbor Report Completed */
+#define NET_EVENT_WIFI_NEIGHBOR_REP_COMP			\
+	(_NET_WIFI_EVENT | NET_EVENT_WIFI_CMD_NEIGHBOR_REP_COMPLETE)
 
 /** Event emitted for Wi-Fi access point enable result */
 #define NET_EVENT_WIFI_AP_ENABLE_RESULT				\
@@ -1407,6 +1437,21 @@ struct wifi_mgmt_ops {
 	 * @return 0 if ok, < 0 if error
 	 */
 	int (*wps_config)(const struct device *dev, struct wifi_wps_config_params *params);
+	/** Trigger candidate scan
+	 *
+	 * @param dev Pointer to the device structure for the driver instance
+	 * @param params Scan parameters
+	 *
+	 * @return 0 if ok, < 0 if error
+	 */
+	int (*candidate_scan)(const struct device *dev, struct wifi_scan_params *params);
+	/** Start 11r roaming
+	 *
+	 * @param dev Pointer to the device structure for the driver instance
+	 *
+	 * @return 0 if ok, < 0 if error
+	 */
+	int (*start_11r_roaming)(const struct device *dev);
 };
 
 /** Wi-Fi management offload API */
@@ -1497,6 +1542,17 @@ void wifi_mgmt_raise_raw_scan_result_event(struct net_if *iface,
  * @param status Disconnect complete status
  */
 void wifi_mgmt_raise_disconnect_complete_event(struct net_if *iface, int status);
+
+#ifdef CONFIG_WIFI_NM_WPA_SUPPLICANT_ROAMING
+/** Wi-Fi management neighbor reports event
+ *
+ * @param iface Network interface
+ * @param inbuf Input buffer of neighbor reports
+ * @param buf_len Lenghth of input buffer
+ */
+void wifi_mgmt_raise_neighbor_rep_recv_event(struct net_if *iface,
+					     char *inbuf, size_t buf_len);
+#endif
 
 /** Wi-Fi management AP mode enable result event
  *

modules/hostap/src/supp_api.h
@@ -118,6 +118,25 @@ int supplicant_11k_cfg(const struct device *dev, struct wifi_11k_params *params)
  */
 int supplicant_11k_neighbor_request(const struct device *dev, struct wifi_11k_params *params);
 
+#ifdef CONFIG_WIFI_NM_WPA_SUPPLICANT_ROAMING
+/** Send candidate scan request
+ *
+ * @param dev Pointer to the device structure for the driver instance.
+ * @param params Scan parameters
+ *
+ * @return 0 if ok, < 0 if error
+ */
+int supplicant_candidate_scan(const struct device *dev, struct wifi_scan_params *params);
+
+/** Send 11r roaming request
+ *
+ * @param dev Pointer to the device structure for the driver instance.
+ *
+ * @return 0 if ok, < 0 if error
+ */
+int supplicant_11r_roaming(const struct device *dev);
+#endif
+
 /**
  * @brief Set Wi-Fi power save configuration
  *